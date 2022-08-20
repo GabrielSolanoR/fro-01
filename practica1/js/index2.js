de7bcc57181e3d98a559650a111d570943e9d3e0
GabrielSolanoR <gcrsolano@gmail.com>
2022-08-20 10:23:34 -0600

    Ejercicios
    Se agregan los primeros 4 ejercicios.

---
+++ practica1/js/index2.js
@@ -0,0 +1,38 @@
+
+//nuevo estandar para variables (es vez de "var")
+//let nombre = "Gabriel";
+
+//Impresion en consola
+//console.log(nombre);
+
+//nombre = "Cristina";
+
+//console.log(nombre);
+
+//Creacion de una constante (no se pueden cambiar y van en mayuscula)
+//const PI = 3.14;
+
+//console.log("El valor de Pi es: ",PI)
+
+
+//Practica
+
+//let num1 = 7;
+//let num2 = 9;
+
+//let resultado1 = num1 + num2;
+//let resultado2 = num1 * num2;
+
+//console.log("Resultado 1 es: ",resultado1);
+//console.log("Resultado 2 es: ",resultado2);
+
+let h2Nombre = document.getElementById("nombre-personaje");
+
+//Imprimir en consola el texto dentro del elemento
+
+console.log("Contenido: ",h2Nombre.textContent);
+console.log("Clase: ",h2Nombre.className);
+
+
+
+
